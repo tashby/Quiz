ae6a6e75595614595d716ce97f752d7124cf8ac8
Ray <raysmithdev@gmail.com>
2016-05-30 14:18:36 +0100

    Mentor questions added

--- Logic.js
+++ Logic.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
   var checker      = document.getElementsByClassName('checker')[0];
   var current      = 0;
 
-     // An object that holds all the questions + possible answers.
-     // In the array --> last digit gives the right answer position
+     // 1. Make an array of object. e.g var allQuestions = [{}, {}];
       allQuestions = {
         'At What temperature do solid objects visibly glow (Celsius)?' : ['525', '125', '32', 0],
 
@@ -19,6 +18,12 @@ $(document).ready(function(){
         'Which is not used to measure temperature?' : ['Kelvin', 'Farenheit', 'Quart', 2]
       };
 
+      // 2. Make a function that loads the questions
+
+      // 3. Make a function that checks the answers
+
+      // 4. Display the current users score based on the amount of questions they got right
+
        function loadQuestion(current) {
   // Loads all the question into the questionArea
   // It grabs the current question based on the 'current'-variable
@@ -29,11 +34,10 @@ $(document).ready(function(){
     questionArea.innerHTML = question;
   }
 
-  
+
   // Start the quiz right away
   loadQuestion(current);
   loadAnswers(current);
 
 
 });
-
